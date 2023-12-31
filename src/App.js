2fd34e37497eacfc15501588105e5417e515f6de
jayannbal <jayannangela.balatero@tup.edu.ph>
2023-12-31 15:55:36 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Set to Zero</button>
+      <h2> COUNTER </h2>
       <h1> {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(0)}>Set to Zero</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
     </div>
   );
 }
